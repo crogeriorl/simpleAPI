b1c28a16621962f05aaa66dfedcffbb175fc3fbd
unknown <daniel.silva3320@gmail.com>
2022-07-29 18:37:13 -0300

    feat: return error

--- src/useCase/createUser/CreateUserController.ts
+++ src/useCase/createUser/CreateUserController.ts
@@ -5,16 +5,19 @@ export class CreateUserController {
     async handle(request: Request, response: Response) {
         const { name, email, password } = request.body;
         const createUserUseCase = new CreateUserUseCase();
-
-        const createPerson = await createUserUseCase.execute({
-            name,
-            email,
-            password,
-        });
-        const result = {
-            name: createPerson.name,
-            email: createPerson.email,
-        };
-        return response.status(201).json(result);
+        try {
+            const createPerson = await createUserUseCase.execute({
+                name,
+                email,
+                password,
+            });
+            const result = {
+                name: createPerson.name,
+                email: createPerson.email,
+            };
+            return response.status(201).json(result);
+        } catch (Error) {
+            return response.status(400).json(Error.message);
+        }
     }
 }
